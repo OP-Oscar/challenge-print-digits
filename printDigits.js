1cc90222ac53242c3715db0b3a907e8414d3b848
OP-Oscar <oscarpena07@outlook.com>
2023-04-20 09:55:22 -0500

    initial commit

---
+++ printDigits.js
@@ -0,0 +1,56 @@
+// Print Digits Backwards
+
+// Difficulty
+// Medium
+// Concepts
+// Logic, Math
+
+// Write a function called printDigits that takes in one argument, num. When given an integer, the function should print (console.log) each digit in reverse order, starting with the ones place. Do not do this by just turning the number into a string and reversing it. Solve the problem using math. (Hint: look up while loops, and don’t forget about the % operator.)
+
+// For example, if you were given 1 you should simply print 1, if given 314 you should print 4, 1, 3, and if given 12 you should print 2, 1:
+
+// printDigits(1)
+// // 1
+
+// printDigits(314)
+// // 4
+// // 1
+// // 3
+
+// printDigits(12)
+// // 2
+// // 1
+
+
+
+// ***************** CODE HERE  ***************** 
+
+
+function printDigits(num){
+    let rem = Math.abs(num); // => variable to hold value as it changes
+    while (rem%10 > 0){ // => loop to keep going 
+    console.log(rem % 10)
+    rem = Math.floor(rem/10)
+    }
+}
+
+
+// printDigits(1)
+// // 1
+
+// printDigits(314)
+// // 4
+// // 1
+// // 3
+
+// printDigits(12)
+// // 2
+// // 1
+
+printDigits(-3895)
+// // 5
+// // 9
+// // 8
+// // 3
+
+console.log(Math.abs(3895))
